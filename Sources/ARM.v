485fbaafca839a9c1761d938fd44d9824443611a
dlimyh98 <dlimyh98@LAPTOP-51RINPU4.localdomain>
2022-09-18 15:42:55 +0800

    complete ARM.v

--- Sources/ARM.v
+++ Sources/ARM.v
@@ -39,16 +39,16 @@
 module ARM(
     input CLK,
     input RESET,
-    //input Interrupt,  // for optional future use
+    //input Interrupt,       // for optional future use
     input [31:0] Instr,
     input [31:0] ReadData,
     output MemWrite,
     output [31:0] PC,
     output [31:0] ALUResult,
-    output [31:0] WriteData
+    output [31:0] WriteData  // for checking what is at RD2
     );
     
-    // RegFile signals
+    /************ RegFile signals ************/
     //wire CLK ;
     wire WE3 ;
     wire [3:0] A1 ;
@@ -59,12 +59,12 @@ module ARM(
     wire [31:0] RD1 ;
     wire [31:0] RD2 ;
     
-    // Extend Module signals
+    /************ Extend Module signals ************/
     wire [1:0] ImmSrc ;
     wire [23:0] InstrImm ;
     wire [31:0] ExtImm ;
     
-    // Decoder signals
+    /************ Decoder signals ************/
     wire [3:0] Rd ;
     wire [1:0] Op ;
     wire [5:0] Funct ;
@@ -79,7 +79,7 @@ module ARM(
     //wire [1:0] ALUControl ;
     //wire [1:0] FlagW ;
     
-    // CondLogic signals
+    /************ CondLogic signals ************/
     //wire CLK ;
     wire PCS ;
     wire RegW ;
@@ -91,40 +91,87 @@ module ARM(
     wire PCSrc ;
     wire RegWrite ; 
     //wire MemWrite
-       
-    // Shifter signals (no Register-shifted Register support yet)
+    
+    /************ Shifter signals (no Register-shifted Register support yet) ************/   
     wire [1:0] Sh ;
     wire [4:0] Shamt5 ;
     wire [31:0] ShIn ;
     wire [31:0] ShOut ;
-    assign ShIn = RD2;
-    assign Instr[6:5] = Sh;
-    assign Instr[11:7] = Shamt5;
     
-    // ALU signals
+    /************ ALU signals ************/
     wire [31:0] Src_A ;
     wire [31:0] Src_B ;
     wire [1:0] ALUControl ;
-    //wire [31:0] ALUResult ;
+   // wire [31:0] ALUResult ;
     wire [3:0] ALUFlags ;
     
-    // ProgramCounter signals
+    /************ ProgramCounter signals ************/
     //wire CLK ;
     //wire RESET ;
     wire WE_PC ;    
     wire [31:0] PC_IN ;
     //wire [31:0] PC ; 
-        
-    // Other internal signals here
+     
+    /************ Other internal signals ************/    
     wire [31:0] PCPlus4 ;
     wire [31:0] PCPlus8 ;
     wire [31:0] Result ;
+    assign PCPlus4 = PC + 4;
+    assign PCPlus8 = PC + 8;
+    assign Result = (MemtoReg == 1'b0) ? ALUResult : ReadData;
     
-    // datapath connections here
+    
+    /************ Implement datapath connections ************/
     assign WE_PC = 1 ; // Will need to control it for multi-cycle operations (Multiplication, Division) and/or Pipelining with hazard hardware.
     
-    assign Src_B = ALUSrc ? ExtImm : ShOut;
+    // RegFile connections
+    assign WE3 = RegWrite;
+    assign A1 = (RegSrc[0] == 1'b0) ? Instr[19:16] : 4'd15;
+    assign A2 = (RegSrc[1] == 1'b0) ? Instr[3:0] : Instr[16:12];
+    assign A3 = Instr[16:12];
+    assign WD3 = Result;
+    assign R15 = PCPlus8;
+     // RD1 and RD2 computed inside RegFile, then used in ALU and Shifter
     
+    // ExtendModule connections
+    assign InstrImm = Instr[23:0];
+     // ImmSrc already connected from Decoder to ExtendModule
+     // ExtImm already connected from Decoder to ALU
+     
+     // Decoder connections
+    assign Rd = Instr[15:12];
+    assign Op = Instr[27:26];
+    assign Funct = Instr[25:20];
+     // PCS, RegW, MemW, NoWrite, FlagW already connected from Decoder to CondLogic
+     // ALUControl already connected from Decoder to ALU
+     // MemtoReg, ALUSrc, ImmSrc, RegSrc used as multiplexer    
+     
+    // CondLogic connections
+    assign Cond = Instr[31:28];
+     // PCS, RegW, NoWrite, MemW, FlagW already connected from Decoder to CondLogic
+     // ALUFlags already connected from ALU to CondLogic
+     // PCSrc used as multiplexer
+     // RegWrite already connected to Decoder (WE3)
+     // MemWrite already connected to ARM.v's output
+     
+    // Shifter connections
+    assign Instr[6:5] = Sh;
+    assign Instr[11:7] = Shamt5;
+    assign ShIn = RD2;
+     // ShOut already connected from Shifter to ALU
+    
+    // ALU connections
+    assign Src_A = RD1;
+    assign Src_B = (ALUSrc == 1'b0) ? ShOut : ExtImm;
+     // ALUControl already connected from Decoder to ALU
+     // ALUResult already connected from ALU to ARM.v's output
+     // ALUFlags already connected from ALU to CondLogic
+     
+   // ProgramCounter connections
+   assign PC_IN = (PCSrc == 1'b0) ? PCPlus4 : Result;
+  
+   
+   /************ Instantations ************/
     
     // Instantiate RegFile
     RegFile RegFile1( 
