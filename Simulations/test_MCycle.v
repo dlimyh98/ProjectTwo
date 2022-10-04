0f29fa46f2ba1f40e7259f8a3d7631bca1667104
Razer-PC\Edmund Tan <edmundexe98@gmail.com>
2022-10-04 21:59:33 +0800

    add test cases

--- Simulations/test_MCycle.v
+++ Simulations/test_MCycle.v
@@ -104,42 +104,93 @@ module test_MCycle(
         Start = 1'b0 ;
         #10 ;
         
-        
-        MCycleOp = 2'b10 ;      // signed div
+        // Test cases for Divide
+        // 10: signed, 11: unsigned
+        //
+        // -ve / +ve
+        MCycleOp = 2'b10 ;      
         // -4 / 3 = -1 R -1 or 1111 R 1111
         Operand1 = 4'b1100 ;        // 0xC
         Operand2 = 4'b0011 ;        // 0x3
         Start = 1'b1 ;
-
         wait(Busy) ; 
         wait(~Busy) ; 
         #10 ;
         Start = 1'b0 ;
         #10 ;
         
-        
-        MCycleOp = 2'b10 ;      // signed div
+        // -ve / -ve
+        MCycleOp = 2'b10 ;      
         // -6 / -4 = 1 R -2 or 0001 R 1111
-        Operand1 = 4'b1010 ;
-        Operand2 = 4'b1100 ;
+        Operand1 = 4'b1010 ;        // 0xA
+        Operand2 = 4'b1100 ;        // 0xC
         Start = 1'b1 ;
-
         wait(Busy) ; 
         wait(~Busy) ; 
         #10 ;
         Start = 1'b0 ;
         #10 ;
         
-        
-        MCycleOp = 2'b11 ;      // unsigned div
+        // +ve / +ve
+        MCycleOp = 2'b11 ;      
         // 8 / 4 = 2 R 0 or 0010 R 0000
-        Operand1 = 4'b1000 ;
-        Operand2 = 4'b0100 ;
+        Operand1 = 4'b1000 ;        // 0x8
+        Operand2 = 4'b0100 ;        // 0x4
         Start = 1'b1 ;
-
         wait(Busy) ; 
         wait(~Busy) ; 
         Start = 1'b0 ;
+        
+        // +ve / -ve
+        MCycleOp = 2'b10 ;      
+        // 3 / -2 = 1 R -1 or 0001 R 1111
+        Operand1 = 4'b0011 ;        // 0x3
+        Operand2 = 4'b1110 ;        // 0xE
+        Start = 1'b1 ;
+        wait(Busy) ; 
+        wait(~Busy) ; 
+        Start = 1'b0 ;
+        
+        // Divide 0 (unsigned)
+        MCycleOp = 2'b11 ;      
+        // 0 / 5 = 0 R 5 or 0000 R 0101
+        Operand1 = 4'b0000 ;        // 0x0
+        Operand2 = 4'b0101 ;        // 0x5
+        Start = 1'b1 ;
+        wait(Busy) ; 
+        wait(~Busy) ; 
+        Start = 1'b0 ;
+        
+        // Divide 0 (signed)
+        MCycleOp = 2'b10 ;      
+        // 0 / -2 = 0 R -2 or 0000 R 1110 
+        Operand1 = 4'b0000 ;        // 0x0
+        Operand2 = 4'b1110 ;        // 0xE
+        Start = 1'b1 ;
+        wait(Busy) ; 
+        wait(~Busy) ; 
+        Start = 1'b0 ;
+        
+        // Divisor > Dividend
+        MCycleOp = 2'b11 ;      
+        // 4 / 8 = 0 R 8 or 0000 R 1000
+        Operand1 = 4'b0100 ;        // 0x4
+        Operand2 = 4'b1000 ;        // 0x8
+        Start = 1'b1 ;
+        wait(Busy) ; 
+        wait(~Busy) ; 
+        Start = 1'b0 ;
+        
+        // Divide by whole (signed)
+        MCycleOp = 2'b10 ;      
+        // -4 / -4 = 1 R 0 or 0001 R 0000
+        Operand1 = 4'b1100 ;    // 0xC
+        Operand2 = 4'b1100 ;    // 0xc
+        Start = 1'b1 ;
+        wait(Busy) ; 
+        wait(~Busy) ; 
+        Start = 1'b0 ;
+        
     end
      
     // GENERATE CLOCK       
